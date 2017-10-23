2b3bb06fa12230f42c6f71d2804b2edb4c97c73c
esteban1234 <tva_libra@hotmail.com>
2017-10-23 12:50:04 -0500

    index iniciado

--- index.php
+++ index.php
@@ -2,146 +2,78 @@
 <?php include 'vistas/encabezado.php' ?>
 <body>
 
-<div class="linea">
-	<p><i class="fa fa-phone" aria-hidden="true"></i> PENDIENTE | COTIZAMOS SIN COSTO &nbsp;&nbsp;&nbsp;&nbsp; <i class="fa fa-envelope-o" aria-hidden="true"></i> info@construccionesduncor.com</p>
+<div class="menu">
+  <nav>
+    <ul>
+      <a class="selector" id="select" href="">INICIO</a>
+      <a class="selector" href="">NOSOTROS</a>
+      <a class="selector" href="">SERVICIOS</a>
+      <a class="selector" href="">CONTACTO</a>
+    </ul>
+  </nav>
 </div>
-
 <header>
-	<figure class="logo pull-left">
-		<img src="img/duncor.png" class="img-responsive" alt="DUNCOR">
-	</figure>
-	<nav class="pull-right">
-		<ul>
-			<a class="selector" id="select" href="index.php"><i class="fa fa-bars" aria-hidden="true"></i> INICIO</a>
-			<a class="selector" href="vistas/nosotros.php">NOSOTROS</a>
-			<a class="selector" href="vistas/servicios.php">SERVICIOS</a>
-			<a class="selector" href="vistas/contacto.php">CONTACTO</a>
-		</ul>
-	</nav>
+  <div class="container">
+    <figure class="logo pull-left">
+      <img src="img/lotan.png" alt="LOTAN" class="img-responsive">
+    </figure>
+
+    <div class="text-h pull-right">
+      <p class="pull-left correo-h"><i style="color: #F6A41D;" class="fa fa-envelope-o" aria-hidden="true"></i> info@construccioneslotan.com</p>
+      <p class="pull-right horario-h"><i style="color: #F6A41D;" class="fa fa-clock-o" aria-hidden="true"></i> Lunes a Viernes de 9 am a 6 pm</p>
+    </div>
+  </div>
 </header>
-
 <div class="banner">
-	<p>Diseñamos y Construimos el hogar que tú y tu familia necesitan.</p>
+  <p>CONSTRUIMOS <br> RESPONSABLEMENTE</p>
+  <div class="boton">
+    <a href="#" class="btn btn-warning btn-lg">CONTACTANOS</a>
+  </div>
 </div>
 
-<h1 class="text-center tit">El <b style="color: #7A170F;">hogar</b> que tú familia y tu se merecen</h1>
-<p class="texto">Te asesoramos desde la elección del terreno y los trámites, hasta el diseño y construcción de tu hogar.</p>
-
-<div class="banner2">
-	<div class="fondo">
-		<h1 class="titbanner">¿Cómo podemos ayudarte?</h1>
-		<hr> <br><br><br>
-    <div class="section">
-      <div class="container">
-        <div class="row">
-          <div class="col-md-4">
-            <h2 style="color: #fff; text-align: center;">Construcción</h2>
-            <p style="color: #fff; text-align: center;">Construimos de acuerdo a la medida de tu proyecto arquitectónico y en base a tu presupuesto.</p>
-          </div>
-          <div class="col-md-4">
-            <h2 style="color: #fff; text-align: center;">Diseño de Proyecto</h2>
-            <p style="color: #fff; text-align: center;">Te ayudamos a crear un proyecto arquitectónico que se adecue a tus necesidades y presupuesto.</p>
-          </div>
-          <div class="col-md-4">
-            <h2 style="color: #fff; text-align: center;">Trámites</h2>
-            <p style="color: #fff; text-align: center;">Evítate gastar tu tiempo en trámites y permisos de construcción y obras; nosotros nos encargamos de todo para que tú sólo disfrutes y supervises es el proyecto de tu vida.</p>
-          </div>
-        </div>
-      </div>
+<div class="container">
+  <div class="icono-tit-i">
+    <div class="icono">
+      <i style="color: #F6A41D;" class="fa fa-trophy fa-5x" aria-hidden="true"></i>
     </div>
-    <div class="boton">
-    	<a href="vistas/contacto.php" style="text-decoration: none;"><button type="button" class="btn btn-danger btn-lg btn-block">CONTACTANOS</button></a>
-    </div>
-	</div>
+    <p class="tit-i">AMPLIA EXPERIENCIA <br>construyendo nuestro MEXICO</p>
+  </div>
 </div>
-
-<h1 class="text-center tit">EL <b style="color: #7A170F;">HOGAR</b> DE TUS SUEÑOS ES UNA REALIDAD</h1>
-<p class="texto">Cotiza tu proyecto, déjate sorprender con todas las posibilidades que tenemos para ti y tu familia</p> <br><br>	
 
 <div class="section">
       <div class="container">
         <div class="row">
-        </div>
-        <div class="row">
           <div class="col-md-4">
             <figure class="imagen">
-            	
+              
             </figure>
-            <h3>Casa Minimalista</h3>
-            <p>-Desarrollo en una planta <br>
--Acabados de primera calidad <br>
--Pisos de porcelanato <br>
--Ventanería de aluminio corredizo</p>
+            <h2 class="text-center">A Title</h2>
+            <p class="text-center">Lorem ipsum dolor sit amet, consectetur adipisici elit,
+              <br>sed eiusmod tempor incidunt ut labore et dolore magna aliqua.
+              <br>Ut enim ad minim veniam, quis nostrud</p>
           </div>
           <div class="col-md-4">
-            <figure class="imagen2">
-            	
+            <figure class="imagen">
+              
             </figure>
-            <h2>Cocina Clásica</h2>
-            <p>-Mesetas recubiertas de Cuarzo <br>
--Gaveteros de madera de cedro <br>
--Accesorios de acero inoxidable <br>
--Pisos de porcelanato</p>
+            <h2 class="text-center">A title</h2>
+            <p class="text-center">Lorem ipsum dolor sit amet, consectetur adipisici elit,
+              <br>sed eiusmod tempor incidunt ut labore et dolore magna aliqua.
+              <br>Ut enim ad minim veniam, quis nostrud</p>
           </div>
           <div class="col-md-4">
-            <figure class="imagen3">
-            	
+            <figure class="imagen">
+              
             </figure>
-            <h2>Casa Minimalista</h2>
-            <p>
-
--Desarrollada en dos niveles <br>
--Cochera para tres autos <br>
--Acabados de lujo <br>
--Ventanería de aluminio pulido corredizo</p>
-          </div>
-        </div>
-        <div class="row">
-          <div class="col-md-4">
-            <figure class="imagen4">
-            	
-            </figure>
-            <h3>Baño contemporáneo</h3>
-            <p>
-
--Muebles de lujo <br>
--Llaves y accesorios cromados <br>
--Cancelarías de cristal templado <br>
--Recubrimientos de primera calidad</p>
-          </div>
-          <div class="col-md-4">
-            <figure class="imagen5">
-            	
-            </figure>
-            <h3>Casa Minimalista</h3>
-            <p>
-
--Desarrolló en dos plantas <br>
--Acabados de primera calidad <br>
--Pisos de porcelanato <br>
--Ventanería de aluminio corredizo <br>
--Alberca equipada <br>
--Balcones y terrazas</p>
-          </div>
-          <div class="col-md-4">
-            <figure class="imagen6">
-            	
-            </figure>
-            <h2>Loft</h2>
-            <p>
-
--Sala comedor con techos a doble altura <br>
--Acabados de primera calidad <br>
--Pisos cerámicos tipo madera <br>
--Ventanería de aluminio corredizo <br>
--Barandales de acero inoxidable</p>
+            <h2 class="text-center">A title</h2>
+            <p class="text-center">Lorem ipsum dolor sit amet, consectetur adipisici elit,
+              <br>sed eiusmod tempor incidunt ut labore et dolore magna aliqua.
+              <br>Ut enim ad minim veniam, quis nostrud</p>
           </div>
         </div>
       </div>
     </div>
-    <div class="boton" style="margin-bottom: 5%; margin-top: 5%;">
-    	<a href="vistas/contacto.php" style="text-decoration: none;"><button type="button" class="btn btn-danger btn-lg btn-block">CONTACTANOS</button></a>
-    </div>
+
 
 <a class="go-top" href="#">Subir</a>
 <?php include 'vistas/pie.php' ?>
